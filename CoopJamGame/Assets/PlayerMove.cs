12866899612a3ec58b4613ac8d63e56f8e190501
Genaro <alexandergenaro@gmail.com>
2016-04-23 15:50:23 -0400

    even more stuff yay

--- CoopJamGame/Assets/PlayerMove.cs
+++ CoopJamGame/Assets/PlayerMove.cs
@@ -8,17 +8,23 @@ public class PlayerMove : MonoBehaviour {
 
     public int MinionID;
 
+    public float MaxDist;
+
    // public int currMin;
+    public GameObject partner;
 
     public GameData gamestats;
 
     public Vector3 velocity;
 
     private Rigidbody rb;
+
+    public bool dontMove;
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        dontMove = false;
 	}
 	
 	// Update is called once per frame
@@ -39,6 +45,21 @@ public class PlayerMove : MonoBehaviour {
                 jump = 50;
             }
             rb.AddForce(new Vector3(moveHoriz, jump, moveVert * 0.5f) * speed);
+
+            if (Input.GetKeyDown(KeyCode.C))
+                dontMove = !dontMove;
+        }
+        else
+        {
+            float dist = Vector3.Distance( transform.position, partner.transform.position);
+
+            if(dist > MaxDist && !dontMove)
+            {
+                if (transform.position.x > partner.transform.position.x)
+                    rb.AddForce(-speed, 0, 0);
+                else
+                    rb.AddForce(speed, 0, 0);
+            }
         }
       
     }
