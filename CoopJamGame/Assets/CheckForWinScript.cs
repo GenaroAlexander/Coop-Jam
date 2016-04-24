0dfa8a129fb4ed4337781790b63ea0a7a5f6cf2b
Genaro <alexandergenaro@gmail.com>
2016-04-24 00:46:48 -0400

    made win condition

---
+++ CoopJamGame/Assets/CheckForWinScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class CheckForWinScript : MonoBehaviour {
+
+
+    public WinPlatformScript redTrig, blueTrig;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(redTrig.Triggered && blueTrig.Triggered)
+        {
+            SceneManager.LoadScene("win screen");
+        }
+
+	}
+}
