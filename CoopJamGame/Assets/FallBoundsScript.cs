3919a534d9f1b717b2b32c8b71734a17eef94e49
Genaro <alexandergenaro@gmail.com>
2016-04-24 15:37:02 -0400

    adding new stuffz

---
+++ CoopJamGame/Assets/FallBoundsScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallBoundsScript : MonoBehaviour {
+
+
+    public Vector3 resetPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = resetPos;
+    }
+	
+}
