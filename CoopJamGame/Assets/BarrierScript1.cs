ced13a286cf5b181dbd0583555e6170ed4d52b3d
rmstanley89 <rmstanley89@gmail.com>
2016-04-24 18:14:09 -0400

    changes

--- CoopJamGame/Assets/BarrierScript1.cs
+++ CoopJamGame/Assets/BarrierScript1.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class BarrierScript1 : MonoBehaviour {
-
-    public bool move;
-
+	float rotationval;
+	public bool move;
     public Vector3 endPos;
 
 	// Use this for initialization
 	void Start () {
         move = false;
+		rotationval = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,12 +18,13 @@ public class BarrierScript1 : MonoBehaviour {
         if(move)
         {
             //transform.position += 
-
+			rotationval += 90 * Time.deltaTime * 2;
             Vector3 phPos = endPos - transform.position;
-            transform.position += phPos * Time.deltaTime;
+            transform.position += phPos * Time.deltaTime *2;
 
-            transform.Rotate(new Vector3(0, 0, 1), -90 * Time.deltaTime);
-
+			if (rotationval < 90) {
+				transform.Rotate (new Vector3 (0, 0, 1), -90 * Time.deltaTime * 2);
+			}
             if (Vector3.Distance(transform.position, endPos) < 6)
                 move = false;
         }
