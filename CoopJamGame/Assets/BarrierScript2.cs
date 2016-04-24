3e8b30f135268149c68654e815fe5f67b6d587dc
Genaro <alexandergenaro@gmail.com>
2016-04-23 20:42:04 -0400

    cam stuff

---
+++ CoopJamGame/Assets/BarrierScript2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierScript2 : MonoBehaviour {
+
+	// Use this for initialization
+
+    public bool move;
+
+    float rotVal;
+
+	void Start () {
+        move = false;
+        rotVal = 90;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(move)
+        {
+            float rotAmt = 90 * Time.deltaTime;
+            rotVal -= rotAmt;
+            transform.Rotate(new Vector3(0, 0, 1), rotAmt);
+
+            if (rotVal <= 0)
+                move = false;
+        }
+
+	}
+}
--- CoopJamGame/Assets/CamScript.cs
+++ CoopJamGame/Assets/CamScript.cs
@@ -9,10 +9,13 @@ public class CamScript : MonoBehaviour
     public GameData gamestats;
     public Vector3 offset;
 
+    bool sideView;
+
 	// Use this for initialization
 	void Start () 
     {
-	
+       // transform.Rotate(new Vector3(0, 1, 0), 90);
+        sideView = true;
 	}
 	
 	// Update is called once per frame
@@ -27,4 +30,20 @@ public class CamScript : MonoBehaviour
              transform.position = thickPlayer.transform.position + offset;
          }
 	}
+
+   public  void SwitchPersp()
+    {
+        sideView = !sideView;
+
+        if(sideView)
+        {
+            offset = new Vector3(0, 2, -15);
+            transform.Rotate(new Vector3(0, 1, 0), -90);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 1, 0), 90);
+            offset = new Vector3(-15, 2, 0);
+        }
+    }
 }
