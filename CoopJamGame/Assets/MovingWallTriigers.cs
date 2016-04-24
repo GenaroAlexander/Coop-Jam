0e8cdb35a1b73082e7525a3237c237b0f92b1faa
rmstanley89 <rmstanley89@gmail.com>
2016-04-24 15:41:30 -0400

    Scripts and material

---
+++ CoopJamGame/Assets/MovingWallTriigers.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingWallTriigers : MonoBehaviour {
+
+	public GameObject wallPart1;
+	public GameObject wallPart2;
+	public GameObject wallPart3;
+
+	public bool triggered;
+	// Use this for initialization
+	void Start () {
+		triggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		void OnTriggerEnter(){
+
+			if (!triggered) {
+				triggered = true;
+
+				wallPart1.SetActive(false);
+				wallPart2.SetActive(false);
+				wallPart3.SetActive(false);
+			}
+	}
+}
---
+++ CoopJamGame/Assets/WallMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMovement : MonoBehaviour {
+
+	public GameObject Wall1;
+	public GameObject Wall2;
+	public GameObject Wall3;
+	public GameObject Trigger;
+
+	float length;
+	float lifetime;
+	float speed;
+
+	void Start(){
+		length = 5;
+		speed = 10;
+		lifetime = 0;
+	}
+	void Update()
+	{
+		lifetime += Time.deltaTime;
+		if (lifetime >= length) {
+			lifetime = 0;
+			speed *= -1;
+		}
+
+		Vector3 Poshelp = Wall1.transform.position;
+		Poshelp.x += speed * Time.deltaTime;
+		Wall1.transform.position = Poshelp;
+
+		Poshelp = Wall2.transform.position;
+		Poshelp.x += speed * Time.deltaTime;
+
+
+		Wall2.transform.position = Poshelp;
+
+		Poshelp = Wall3.transform.position;
+		Poshelp.x += speed * Time.deltaTime;
+
+		Wall3.transform.position = Poshelp;
+
+		Poshelp = Trigger.transform.position;
+		Poshelp.x += speed * Time.deltaTime;
+
+		Trigger.transform.position = Poshelp;
+	}
+}
