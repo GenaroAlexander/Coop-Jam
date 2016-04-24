1aa05caecec9826ab499e130336df66a66642163
rmstanley89 <rmstanley89@gmail.com>
2016-04-23 21:59:55 -0400

    Load Level script

---
+++ CoopJamGame/Assets/Loadlvl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Loadlvl : MonoBehaviour {
+
+    public void Loadlevel(string name)
+    {
+
+
+        SceneManager.LoadScene(name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    
+    void Update()
+    {
+    }
+}
+
